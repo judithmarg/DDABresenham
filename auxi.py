b51bdad5dce1ade0c633951e3dfc55037495382c
Judith <judithmargaritapacollalli@gmail.com>
2022-09-24 23:56:05 -0400

    nueva implementacion

--- auxi.py
+++ auxi.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import time
 
 def dda(x0,y0,x1,y1, grosor):
     blank = np.zeros((1080,1920,3), dtype="uint8")
@@ -13,17 +12,30 @@ def dda(x0,y0,x1,y1, grosor):
         steps = abs(dy)
     xs = dx / steps
     ys = dy / steps 
-    for i in range(0, steps+grosor):    
-        print('x = ',round(x), 'y = ', round(y))   
-        blank[round(x-grosor/2):round(x+grosor/2), round(y-grosor/2):round(y+grosor/2)] = (0,255,10)
+    print(steps)
+    for i in range(0, steps):
+        print('x = ',round(x), 'y = ', round(y))         
+        blank[roundDif(x-grosor/2, grosor):roundDif(x+grosor/2,grosor), roundDif(y-grosor/2,grosor):roundDif(y+grosor/2,grosor)] = (0,255,10)
+        if ys == 1: 
+            y += grosor
+            x += grosor*xs
+        else:
+            x += grosor 
+            y += grosor*ys
         
-        y += grosor+ys
-        x += grosor+xs 
     cv.imshow("Green", blank)
     cv.waitKey(0)
     
+def roundDif(n, c):
+    if(n %c < c/2):
+        result = n
+    else:
+        result = n-(n%c)+c
+    return int(result)
+    
 def main():
-    dda(150,125,170,180,5)
+    dda(56,100,90,160,14)
 
 if __name__ == '__main__':
     main()
+
